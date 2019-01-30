a9b7d7d376aea48a6c5cce003d1a3677e9791e42
Pencil-L <15047145261@163.com>
2019-01-30 12:05:00 +0800

    提交文件

---
+++ src/main/java/AtomicTest11.java
@@ -0,0 +1,13 @@
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈多线程使用AtomicLong统计0的个数〉
+ *
+ * @author lixuanfeng
+ * @create 2019/1/30
+ * @since 1.0.0
+ */
+public class AtomicTest11 {
+
+    
+
+}
---
+++ src/main/java/ForContentTest9.java
@@ -0,0 +1,28 @@
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈〉
+ *
+ * @author lixuanfeng
+ * @create 2019/1/29
+ * @since 1.0.0
+ */
+public class ForContentTest9 {
+
+    static final int LINE_NUM = 1024;
+    static final int COLUM_NUM = 1024;
+
+    public static void main(String[] args) {
+        long[][] array = new long[LINE_NUM][COLUM_NUM];
+
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < LINE_NUM; i++){
+            for (int j = 0; j < COLUM_NUM; j++){
+                array[i][j] = i*2+j;
+            }
+        }
+
+        long endTime = System.currentTimeMillis();
+        long cacheTime = endTime - startTime;
+        System.out.println("cache time:" + cacheTime);
+    }
+}
